051cf4660ba0e50141e337dbe01f09bda054b592
federicomanni97 <manni_federico@libero.it>
2023-12-15 14:26:28 +0100

    hello world added

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $data = [
+        'hello' => 'Hello',
+        'name' => 'World!',
+    ];
+    return view('home', $data);
 });
